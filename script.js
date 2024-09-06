4cd925dcfdee01247f899a231f470c6ee8217e9e
ummyhajj <ummyhajj222@gmail.com>
2024-09-06 04:34:22 -0700

    I added event listeners,and it listen to the arrow keys then perform a function of changeDirection

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ var rows = 20;
 var cols = 20;
 var board;
 var context;
+//snakes head
 var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
+
+var velocityX = 0;
+var velocityY = 0;
 //Food
 var foodX 
 var foodY 
@@ -17,22 +21,44 @@ window.onload = function(){
     
     placeFood();
     document.addEventListener('keyup', changeDirection);
-    update();
+    // update()
+    setInterval(update,1000/10);// Every 100 milliseceond,it is going to run the update function
    
 }
 function update(){
     context.fillStyle = '#000';
     context.fillRect(0,0, board.width, board.height);
 
-    context.fillStyle = 'line';
-    context.fillRect(snakeX,snakeY, blockSize, blockSize);
-    // The parameters entails the x(snakeX) and y(snakeY) coordinates,width(blockSize) and height(blockSize)
-
     context.fillStyle = 'red';
     context.fillRect(foodX,foodY, blockSize, blockSize);
-}
-function changeDirection(){
+    if(snakeX === foodX && snakeY === foodY){
 
+    }
+    context.fillStyle = 'lime';
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX,snakeY, blockSize, blockSize);
+    // The parameters entails the x(snakeX) and y(snakeY) coordinates,width(blockSize) and height(blockSize)
+    
+}
+function changeDirection(e){
+    if (e.code =='ArrowUp' && velocityY !=1){
+        velocityX =0;
+        velocityY = -1;
+    }
+    else if (e.code =='ArrowDown' && velocityY !=-1){
+        velocityX =0;
+        velocityY = 1;
+    }
+    else if (e.code =='ArrowLeft'&& velocityX !=-1){ 
+        velocityX =-1;
+        velocityY = 0;
+    }
+    else if (e.code =='ArrowRight' && velocityX !=1){
+        velocityX =1;
+        velocityY = 0;
+    }
+    
 }
 function placeFood(){
     foodX = Math.floor(Math.random() * cols) * blockSize;
